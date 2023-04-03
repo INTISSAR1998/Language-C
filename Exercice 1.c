214eb56e423a8b95009e90cd685a36b437e08052
Intissar EL AHMER <intissarelahmer1998@gmail.com>
2023-04-03 12:09:58 +0100

    Rename exe 1.c to Exercice 1.c

--- Exercice 1.c
+++ Exercice 1.c
@@ -2,13 +2,13 @@
 #define NMAX 10
 main ()
 { int i, j;
-/*affichage ligne en-tête */
+/*affichage ligne en-tÃªte */
 printf("\tI");
 for (j=1;j<=NMAX;j++) printf ("%4d",j);
 printf("\n");
 for(j=1;j<=NMAX;j++) printf("_ _ _ _ _");
 printf ("\n");
-/* affichage des différentes lignes */
+/* affichage des diffÃ©rentes lignes */
 for(i=1;i<=NMAX;i++)
 {printf ("%4d\tI",i);
 for (j=1;j<=NMAX; j++)
